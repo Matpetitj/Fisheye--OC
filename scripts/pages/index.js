c3efbcdc67a8b6602cec3591db26c4ff664f1e33
Mathis Petitjean <100531763+Matpetitj@users.noreply.github.com>
2023-11-19 16:50:45 +0100

    UPDATE : adjust the photographers factory pattern, add the alert to the fetch's catch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,24 +1,24 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        return await fetch("data/photographers.json")
-        .then(function (res) {
+        return await fetch("./data/photographers.json")
+        .then((res) => {      
             if (res.ok) {
                 return res.json();
             }
         })
-        .then(function (value) {
-            //console.log(value)
+        .then((value) => {
             return (value)
         })
         .catch(function (err) {
             console.log(err);
+            alert("Une erreur est survenue lors du chargement des données. Veuillez contacter l\'administrateur du site !");
         })
     }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
-
+        photographersSection.innerHTML="";
         photographers.forEach((photographer) => {
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,21 +1,26 @@
 function photographerTemplate(data) {
     const { name, portrait, id, tagline, city, country, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `./assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
 
         const article = document.createElement('article');
-        const img = document.createElement('img');
+
+        const a = document.createElement('a');
+        a.href = `./photographer.html?id=${id}`;
+        a.setAttribute('aria-label', 'Lien du profil de ' + name);
+        article.appendChild(a);
+
         const imgContainer = document.createElement('div');
+        
+        const img = document.createElement('img');
         img.setAttribute("src", picture);
         img.setAttribute("alt", 'photo de ' + name);
+       
         const h2 = document.createElement('h2');
         h2.textContent = name;
-        const a = document.createElement('a');
-        // a.href = `photographer.html?id=${id}`;
-        a.href = `#`;
-        a.setAttribute('aria-label', 'Lien du profil de ' + name);
+       
         const location = document.createElement('h3');
         location.textContent = city + ', ' + country;
         const text = document.createElement('p');
@@ -34,12 +39,14 @@ function photographerTemplate(data) {
         imgContainer.appendChild(img);
         a.appendChild(imgContainer);
         a.appendChild(h2);
-        article.appendChild(a);
-        article.appendChild(location);
-        article.appendChild(text);
-        article.appendChild(dayPrice)
+        
+        a.appendChild(location);
+        a.appendChild(text);
+        a.appendChild(dayPrice)
 
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    function getHeaderPhotographer(){}
+    return { getUserCardDOM , getHeaderPhotographer}
 }
