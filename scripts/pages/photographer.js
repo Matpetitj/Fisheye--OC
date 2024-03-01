6bd46bf0353672de39f1a215228deda67ba60c0b
Mathis Petitjean <100531763+Matpetitj@users.noreply.github.com>
2024-03-01 13:20:36 +0100

    Correction on filter feature, put in html instead of js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -122,127 +122,57 @@ async function init() {
 
 // Filtres
 
-const filterContainer = document.getElementById('filter-container');
-
-const mediaFilters = document.createElement('section');
-mediaFilters.classList.add('media-filters-container');
-
-const dropdown = document.createElement('div');
-dropdown.classList.add('dropdown');
-dropdown.setAttribute("role", "menubar");
-dropdown.setAttribute("tabIndex", 0);
-
-dropdown.addEventListener("keydown", function(e) {
-    let dropdownMenu = document.querySelector('.menu-dropdown');
-    if (e.key == "Enter") {
-        dropdownMenu.style.visibility = "visible";
-        dropdownMenu.style.opacity = 1;
-        dropdownMenu.style.transform = "translateY(0%)";
-    };
-    if (e.key == "Escape") {
-        dropdownMenu.style.visibility = "hidden";
-        dropdownMenu.style.opacity = 0;
-        dropdownMenu.style.transform = "translateY(-50%)";
-    };
-});
-
-const menuDropdown = document.createElement('div');
-menuDropdown.classList.add('menu-dropdown');
-
-const Popularite = document.createElement('button');
-Popularite.classList.add('menu-filter');
-Popularite.classList.add('select-popularity');
-Popularite.textContent = "Popularité";
-Popularite.setAttribute("role", "menu");
-Popularite.setAttribute("type", "button");
-Popularite. setAttribute("value", "type00");
-
-const DateFilter = document.createElement('button');
-DateFilter.classList.add("menu-filter");
-DateFilter.classList.add("select-date");
-DateFilter.textContent = "Date";
-DateFilter.setAttribute("role", "menu");
-DateFilter.setAttribute("type", "button");
-DateFilter.setAttribute("value", "type01");
-
-const Titre = document.createElement('button');
-Titre.classList.add("menu-filter");
-Titre.classList.add("select-title");
-Titre.textContent = "Titre";
-Titre.setAttribute("role", "menu");
-Titre.setAttribute("type", "button");
-Titre.setAttribute("value", "type02");
-
-const chevronIcon = document.createElement('i');
-chevronIcon.setAttribute('class', 'chevron-icon fa-sharp fa-solid fa-angle-up');
-
-const emptyButton = document.createElement('div');
-emptyButton.classList.add("empty-button");
-const emptyButton2 = document.createElement('div');
-emptyButton.classList.add("empty-button");
-const emptyButtonWhite = document.createElement('div');
-emptyButton.classList.add("empty-button-white");
-const emptyButtonWhite2 = document.createElement('div');
-emptyButton2.classList.add("empty-button-white");
-
-filterContainer.appendChild(mediaFilters);
-mediaFilters.appendChild(dropdown);
-dropdown.appendChild(Popularite);
-Popularite.appendChild(chevronIcon);
-dropdown.appendChild(menuDropdown);
-menuDropdown.appendChild(emptyButton);
-emptyButton.appendChild(emptyButtonWhite);
-menuDropdown.appendChild(DateFilter);
-menuDropdown.appendChild(emptyButton2);
-emptyButton.appendChild(emptyButtonWhite2);
-menuDropdown.appendChild(Titre);
-
 /* Bloc de tri des cartes */
 
 function sortMedias(listMedia){
     console.log(listMedia);
 
-    Popularite.addEventListener("click", function (event){
+    const popularityBtn = document.getElementById("popularity-btn");
+    const dateBtn = document.getElementById("date-btn");
+    const titleBtn = document.getElementById("title-btn");
+
+    popularityBtn.addEventListener("click", function(event) {
         event.preventDefault();
         event.stopPropagation();
-        listMedia.sort(function (a, b) {
-            if(a.likes < b.likes){
-                return 1;
-            }
-            if(a.likes > b.likes){
-                return -1;
-            };
-            return 0; 
-        });
-        //Commenter si j'utilise l'autre option
-        // displayData(listMedia);
-
-        // Deuxieme façon pour actualiser le dom 
-        reorganizeMedias(listMedia);
+        sort("type00", listMedia);
     });
-
-    DateFilter.addEventListener("click", function(event){
+    dateBtn.addEventListener("click", function(event) {
         event.preventDefault();
         event.stopPropagation();
-        listMedia.sort(function (a, b) {
-            if(a.date < b.date){
-                return -1;
-            }
-            if(a.date > b.date){
-                return 1;
-            };
-            return 0; 
-        });
-        //Commenter si j'utilise l'autre option
-        // displayData(listMedia);
-
-        // Deuxieme façon pour actualiser le dom 
-        reorganizeMedias(listMedia);
-    })
-
-    Titre.addEventListener("click", function(event){
+        sort("type01", listMedia);
+    });
+    titleBtn.addEventListener("click", function(event) {
         event.preventDefault();
         event.stopPropagation();
+        sort("type02", listMedia);
+    });
+}
+
+function sort (type, listMedia) {
+        // Il faut ajouter une condition de vérification de la value selected 
+        if(type=="type00"){
+            listMedia.sort(function (a, b) {
+                if(a.likes < b.likes){
+                    return 1;
+                }
+                if(a.likes > b.likes){
+                    return -1;
+                };
+                return 0; 
+            });
+        }
+        if(type=="type01"){
+            listMedia.sort(function (a, b) {
+                if(a.date < b.date){
+                    return -1;
+                }
+                if(a.date > b.date){
+                    return 1;
+                };
+                return 0; 
+            });
+        }
+        if(type=="type02"){
         listMedia.sort(function (a, b) {
             if(a.title < b.title){
                 return -1;
@@ -252,13 +182,13 @@ function sortMedias(listMedia){
             };
             return 0; 
         });
+    }
 
-        //Commenter si j'utilise l'autre option
-        // displayData(listMedia);
+    //Commenter si j'utilise l'autre option
+    // displayData(listMedia);
 
-        // Deuxieme façon pour actualiser le dom 
-        reorganizeMedias(listMedia);
-    })
+    // Deuxieme façon pour actualiser le dom 
+    reorganizeMedias(listMedia);
 }
 
 function reorganizeMedias(listMedia){
@@ -283,8 +213,8 @@ function reorganizeMedias(listMedia){
         wrapper.insertBefore(nodeSearched, nodeReference);
 
         // Deplacer le node modal recherché sur la lightbox avant l'element de la position de reference 
-        const nodeSearchedModal = arrayCreatedMedias[positionElement];
-        const nodeReferenceModal = arrayCreatedMedias[positionRefence];
+        const nodeSearchedModal = arrayCreatedMediasModal[positionElement];
+        const nodeReferenceModal = arrayCreatedMediasModal[positionRefence];
         wrapperModal.insertBefore(nodeSearchedModal, nodeReferenceModal);
 
         positionRefence++;
